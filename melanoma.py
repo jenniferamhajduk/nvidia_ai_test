29b7bc539898a7ddccd07b9eba3e6fbc323af51e
Jennifer Hajduk <jhajduk@jhajduk-mlt.client.nvidia.com>
2025-05-14 13:46:45 -0700

    init

---
+++ melanoma.py
@@ -0,0 +1,247 @@
+import torch
+import tqdm
+import torchinfo
+import zipfile
+from torch import nn
+from torchinfo import summary
+from torch.utils.data import DataLoader, Dataset
+import torchvision
+from torchvision import datasets, transforms, models
+from pathlib import Path
+from PIL import Image
+import numpy as np
+import matplotlib.pyplot as plt
+import tqdm
+from tqdm.auto import tqdm
+from timeit import default_timer as timer
+from typing import Tuple, Dict, List
+import random
+import os
+import subprocess
+torch.__version__
+
+#Download the dataset
+# Download the dataset
+try:
+    # Create data directory if it doesn't exist
+    os.makedirs("./data", exist_ok=True)
+    
+    # Execute curl command to download the dataset
+    subprocess.run(
+        ["curl", "-L", "-o", "./data/melanoma-cancer-dataset.zip", 
+         "https://www.kaggle.com/api/v1/datasets/download/bhaveshmittal/melanoma-cancer-dataset"],
+        check=True
+    )
+    print("Dataset downloaded successfully.")
+except Exception as e:
+    print(f"Error downloading dataset: {e}")
+
+
+# Unzip the dataset
+# Path to the zip file
+zip_file_path = "./data/melanoma-cancer-dataset.zip"
+extract_to_path = "./data/melanoma-cancer-dataset"
+
+# Extract the zip file
+with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
+    zip_ref.extractall(extract_to_path)
+
+print(f"Extracted to {extract_to_path}")
+
+#Device info
+device = "cuda" if torch.cuda.is_available() else "cpu" 
+print(device)
+nvidia_smi = subprocess.run(["nvidia-smi"], capture_output=True, text=True)
+print(nvidia_smi.stdout)
+
+#Walk through the dataset
+IMAGE_PATH = "./data/melanoma-cancer-dataset"
+def walk_through_dir(dir_path):
+    for dirpath, dirnames, filesnames in os.walk(dir_path):
+        print(f"There are {len(dirnames)} directories and {len(filesnames)} images in '{dirpath}'")
+
+walk_through_dir(IMAGE_PATH)
+
+train_dir = os.path.join(IMAGE_PATH, "train")
+test_dir = os.path.join(IMAGE_PATH, "test")
+
+print(f"Train Directory: {train_dir}")
+print(f"Test Directory: {test_dir}")
+
+
+data_transform = transforms.Compose([
+    transforms.Resize(size=(224, 224)),
+    #transforms.RandomHorizontalFlip(p=0.5), #Augmentation helps with overfitting
+    transforms.ToTensor()
+])
+
+train_data = datasets.ImageFolder(root=train_dir,
+                                  transform=data_transform,
+                                  target_transform=None)
+
+test_data = datasets.ImageFolder(root=test_dir,
+                                 transform=data_transform,
+                                 target_transform=None)
+
+classes = train_data.classes
+classes
+
+class_dict = train_data.class_to_idx
+class_dict
+
+
+class ImageFolderCustom(Dataset):
+    def __init__(self, targ_dir: str, transform=None):
+        self.paths = list(Path(targ_dir).glob("*/*.jpg"))
+        self.transform = transform
+        self.classes, self.class_to_idx = find_classes(targ_dir)
+    
+    def load_image(self, index: int) -> Image.Image:
+        image_path = self.paths[index]
+        return Image.open(image_path)
+    
+    def __len__(self) -> int:
+        return len(self.paths)
+    
+    def __getitem__(self, index: int) -> Tuple[torch.tensor, int]:
+        img = self.load_image(index)
+        class_name = self.paths[index].parent.name
+        class_idx = self.class_to_idx[class_name]
+        
+        if self.transform:
+            return self.transform(img), class_idx
+        else:
+            return img, class_idx
+
+
+train_transforms = transforms.Compose([
+    transforms.Resize(size=(128,128)),
+    transforms.TrivialAugmentWide(num_magnitude_bins=30),
+    transforms.ToTensor()
+])
+test_transforms = transforms.Compose([
+    transforms.Resize(size=(128,128)),
+    transforms.ToTensor()
+])
+
+
+train_data_custom = ImageFolderCustom(targ_dir=train_dir,
+                                      transform=train_transforms)
+
+test_data_custom = ImageFolderCustom(targ_dir=test_dir,
+                                     transform=test_transforms)
+
+
+BATCH_SIZE=32
+NUM_WORKERS = os.cpu_count()
+train_data_loader_custom = DataLoader(dataset=train_data_custom,
+                                      batch_size=BATCH_SIZE,
+                                      num_workers=NUM_WORKERS,
+                                      shuffle=True)
+
+test_data_loader_custom = DataLoader(dataset=test_data_custom,
+                                     batch_size=BATCH_SIZE,
+                                     num_workers=NUM_WORKERS,
+                                     shuffle=False)
+
+model = models.resnet18(pretrained=True)
+for params in model.parameters():
+  params.requires_grad_ = False
+nr_filters = model.fc.in_features
+model.fc = nn.Linear(nr_filters, 2)
+
+model = model.to(device)
+
+
+def train_step(model: torch.nn.Module,
+          dataloader: torch.utils.data.DataLoader,
+          loss_fn: torch.nn.Module,
+          optimizer: torch.optim.Optimizer,
+          device=device):
+    
+    model.train()
+
+    train_loss, train_acc = 0, 0
+    for batch, (X,y) in enumerate(dataloader):
+        X, y = X.to(device), y.to(device)
+        y_pred = model(X)
+        loss = loss_fn(y_pred, y)
+        train_loss += loss.item()
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        y_pred_class = torch.argmax(torch.softmax(y_pred, dim=1), dim=1)
+        train_acc += ((y_pred_class == y).sum().item()/len(y_pred))
+
+    train_loss = train_loss / len(dataloader)
+    train_acc = train_acc / len(dataloader)
+    
+    return train_loss, train_acc
+
+
+
+def test_step(model: torch.nn.Module,
+          dataloader: torch.utils.data.DataLoader,
+          loss_fn: torch.nn.Module,
+          device=device):
+    
+    model.eval()
+    test_loss, test_acc = 0, 0
+
+    with torch.inference_mode():
+        for batch, (X_test, y_test) in enumerate(dataloader):
+            X_test, y_test = X_test.to(device), y_test.to(device)
+            test_pred = model(X_test)
+            loss = loss_fn(test_pred, y_test)
+            test_loss += loss.item()
+            test_pred_labels = test_pred.argmax(dim=1)
+            test_acc += ((test_pred_labels == y_test).sum().item()/len(test_pred_labels))
+
+    test_loss = test_loss / len(dataloader)
+    test_acc = test_acc / len(dataloader)
+
+    return test_loss, test_acc
+
+
+def trainer_model(model: torch.nn.Module,
+            train_dataloader: torch.utils.data.DataLoader,
+            test_dataloader: torch.utils.data.DataLoader,
+            loss_fn: torch.nn.Module,
+            optimizer: torch.optim.Optimizer,
+            epochs: int = 5,
+            device=device):
+    
+    results = {"train_loss": [],
+               "train_acc": [],
+               "test_loss": [],
+               "test_acc": []}
+    
+    for epoch in tqdm(range(epochs)):
+        train_loss, train_acc = train_step(model=model, dataloader=train_dataloader, loss_fn=loss_fn, optimizer=optimizer, device=device)
+        test_loss, test_acc = test_step(model=model, dataloader=test_dataloader, loss_fn=loss_fn, device=device)
+        print(f"Epoch: {epoch} | Train Loss: {train_loss:.4f} | Train Accuracy: {train_acc:.4f} | Test Loss: {test_loss:.4f} | Test Accuracy: {test_acc:.4f}")
+        results["train_loss"].append(train_loss)
+        results["train_acc"].append(train_acc)
+        results["test_loss"].append(test_loss)
+        results["test_acc"].append(test_acc)
+    return results
+    
+
+LEARNING_RATE = 0.0001
+EPOCHS = 25
+loss_fn = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(params=model.parameters(), lr=LEARNING_RATE)
+
+
+
+start_time = timer()
+model_results = trainer_model(model=model,
+                        train_dataloader=train_data_loader_custom,
+                        test_dataloader=test_data_loader_custom,
+                        loss_fn=loss_fn,
+                        optimizer=optimizer,
+                        epochs=EPOCHS,
+                        device=device)
+end_time = timer()
+print(f"Total Training Time: {end_time-start_time:.3f} seconds on {device}")
+
